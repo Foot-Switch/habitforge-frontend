9b33e055662bae209e1736d949c995e920eeed22
Robert Raušel <robert.rausel@ingemark.com>
2024-07-17 08:55:19 +0200

    Color change.

--- lib/application/common/colors/app_colors.dart
+++ lib/application/common/colors/app_colors.dart
@@ -1,5 +1,5 @@
 import 'package:habitforge_frontend/application/common/colors/color_tools.dart';
 
 class AppColors {
-  static final habitForgeGreen = createMaterialColor("98FB98");
+  static final habitForgeGreen = createMaterialColor("2E8B57");
 }
